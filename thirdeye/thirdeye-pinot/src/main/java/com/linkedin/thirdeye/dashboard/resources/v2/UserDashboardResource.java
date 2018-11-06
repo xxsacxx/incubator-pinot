fd1470a7194fa27d8774c5e5b060e5e3b338f977
xhsun <xhsun@linkedin.com>
2018-11-06 09:41:11 -0800

    [TE] user dashboard - hide anomalies if alert is disabled

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/UserDashboardResource.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/UserDashboardResource.java
@@ -61,6 +61,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import org.apache.commons.lang.StringUtils;
+import sun.util.resources.cldr.wae.CalendarData_wae_CH;
 
 
 /**
@@ -172,7 +173,10 @@ public class UserDashboardResource {
     if (StringUtils.isNotBlank(application)) {
       List<AnomalyFunctionDTO> functions = this.functionDAO.findAllByApplication(application);
       for (AnomalyFunctionDTO function : functions) {
-        applicationFunctionIds.add(function.getId());
+        // only show anomalies with active alerts
+        if (function.getIsActive()) {
+          applicationFunctionIds.add(function.getId());
+        }
       }
     }
 
